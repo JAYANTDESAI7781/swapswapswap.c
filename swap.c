01438dcef507bdf437f07799888a9cb1a81bc349
JAYANTDESAI7781 <120478812+JAYANTDESAI7781@users.noreply.github.com>
2023-02-02 22:49:19 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,17 +1,17 @@
-# swapswapswap.c
-# include <stdio.h>
-void swap(int a, int b);
+# pointertofunction
+
+\\ code for pointertofunction
+
+# include <stdio.h
+void _swap(int* a, int *b);
 int main() {
 int x = 3, y = 5;
-//call by value
-swap(x, y);
+_swap(&x, &y);
 printf("x = %d & y = %d\n", x, y);
-
 return 0;
 }
-
-void swap(int a, int b) {
-int t = a;
-a = b;
-b = a;
+void _swap(int* a, int* b) {
+int t = *a;
+*a = *b;
+*b = *a;
 }
